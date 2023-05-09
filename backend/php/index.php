8e341c733a1b04563f7150baff6f1e8c9c9dea27
Christoph <christoph@debian>
2023-05-09 20:57:42 +0200

    add information about muliple workers to the index.php

--- backend/php/index.php
+++ backend/php/index.php
@@ -1,5 +1,8 @@
 <?php
 
+//INFO: For this to work with the inbuild PHP Server, you need to have multiple workers available
+//run: export "PHP_CLI_SERVER_WORKERS=2"
+
 require "./vendor/autoload.php";
 
 use Acceleration\Acceleration;
