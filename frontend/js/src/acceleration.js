59c1ca878f8e04b5c41624c25bdca96c27581a53
Christoph <christoph@debian>
2023-05-11 20:10:54 +0200

    implement custom Acceleration base class for all webcomponents

--- frontend/js/src/acceleration.js
+++ frontend/js/src/acceleration.js
@@ -1,3 +1,5 @@
+//@ts-check
+
 /**
  * The function that loads the correct webcomponent based on the response from the backend
  */
@@ -7,7 +9,7 @@ function route(){
 
     if(!!!main_div.hasAttribute("x-page")) throw "Acceleration Error: The main div is missing the x-page attribute";
     
-    let page_object = JSON.parse(main_div.getAttribute("x-page"));
+    let page_object = JSON.parse(/** @type {string} */ (main_div.getAttribute("x-page")));
 
     //create a new html element and insert it into the main div
     //the name of the element is the component 
@@ -15,12 +17,14 @@ function route(){
 
    if(!!!webcomponent_name) throw "Acceleration Error: The page_object does not include a 'page' attribute!";
 
+   /**@type {AccelerationComponent} */
    let component = document.createElement(webcomponent_name);
 
    //set the props property in the webcomponent
    let props = page_object["props"];
    if(!!!props) throw "Acceleration Error: The page_object does not include a 'props' attribute!";
-   component.props = props;
+
+   component.setProps(props);
 
 
    main_div.appendChild(component);    
@@ -28,3 +32,37 @@ function route(){
 
 //call the route method once the webpage is loaded
 window.addEventListener("DOMContentLoaded",route);
+
+
+
+/**
+ * The baseclass for all Webcomponents that might be routed by Acceleration
+ * @class
+ * @constructor
+ * @public
+ * @abstract
+ * 
+ * @extends HTMLElement
+ */
+class AccelerationComponent extends HTMLElement{
+
+    /**
+     * This property holds all the data the was passed from the backend to the frontend
+     * @type {Object<string,any>}
+     * @protected
+     */
+    props;
+
+    constructor() {
+        super();
+    }
+
+    /**
+     * This function allows to set the props property
+     * @param {Object<string,any>} props 
+     */
+    setProps(props){
+        this.props = props;
+    }
+
+}
