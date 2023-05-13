a080599512f3c218fed76846f2fdf0f30a020182
Christoph <christoph@debian>
2023-05-13 21:50:28 +0200

    extend form example

--- backend/php/index.php
+++ backend/php/index.php
@@ -11,6 +11,18 @@ Acceleration::setBaseView("http://localhost:8080/frontend/js/index.html");
 
 sleep(1);
 
+//validate form data
+if(!!!isset($_GET["name"]) or $_GET["name"]===""){
+    die(Acceleration::render("hello-world",[
+        "errors" => [
+            "name" => "The field name is required!"
+        ]
+    ]));
+}
+//get the form data sent via GET request
+$name = $_GET["name"];
+
+
 die(Acceleration::render("hello-world",[
-    "text" => "Chris"
+    "text" => $name
 ]));
